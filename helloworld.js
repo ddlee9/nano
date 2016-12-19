cfc427459639510edcbba86215a2e69deea03602
Y50 <Y50>
2016-12-19 14:59:31 +0900

    conflict resolved github

--- helloworld.js
+++ helloworld.js
@@ -3,6 +3,6 @@ console.log('Merry Christmas');
 console.log('Tell your world');
 console.log('Tell his world');
 console.log('Tell her world');
-console.log('Tell our world');<<<<<
+console.log('Tell our world');
 console.log('modify github local repository');
 console.log('modify github remote repository');
